cbf20f6b8a3d278714256bf38f5d7ec0c8000588
f4nt <f4nt@f4ntasmic.com>
2012-09-08 19:04:09 -0500

    tons of base and account template cleanup

--- collector/collector/urls.py
+++ collector/collector/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^friends/', include('friends.urls')),
